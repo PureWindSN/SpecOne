c6ad83b65b35ae9f3c2872ffbe625aa151f36b3e
JiaLuKuan <lukuan.jia@ingeek.com>
2021-04-11 17:47:14 +0800

    initial commit

---
+++ TRUNetworking/Classes/WINDNetWorking.h
@@ -0,0 +1,19 @@
+//
+//  WINDNetWorking.h
+//  TRUNetworking_Example
+//
+//  Created by Wind on 2021/4/9.
+//  Copyright © 2021 JiaLuKuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WINDNetWorking : NSObject
+
++ (void)version;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRUNetworking/Classes/WINDNetWorking.m
@@ -0,0 +1,17 @@
+//
+//  WINDNetWorking.m
+//  TRUNetworking_Example
+//
+//  Created by Wind on 2021/4/9.
+//  Copyright © 2021 JiaLuKuan. All rights reserved.
+//
+
+#import "WINDNetWorking.h"
+
+@implementation WINDNetWorking
+
++ (void)version {
+    NSLog(@"");
+}
+
+@end
